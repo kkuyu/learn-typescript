bf3d3772447b67369134fe8bb3ebcf5b06ec158f
kkuyu <devils67@naver.com>
2022-03-04 20:25:55 +0900

    getting-started

---
+++ getting-started/index.js
@@ -0,0 +1,7 @@
+// npm install typescript -g
+// tsc index.ts
+function sumTs(a, b) {
+    return a + b;
+}
+var resultTs = sumTs(10, 20);
+resultTs.toLocaleString();
---
+++ getting-started/index.ts
@@ -0,0 +1,9 @@
+// npm install typescript -g
+// tsc index.ts
+
+function sumTs(a: number, b: number): number {
+  return a + b;
+}
+
+var resultTs = sumTs(10, 20);
+resultTs.toLocaleString();
