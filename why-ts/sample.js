486f732e352686e0c7c62d7ca4176b3517540166
kkuyu <devils67@naver.com>
2022-03-04 20:18:59 +0900

    why-ts: jsdoc, ts-check

--- why-ts/sample.js
+++ why-ts/sample.js
@@ -4,3 +4,15 @@ function sum(a, b) {
 
 var result = sum(10, 20);
 result.toLocaleString();
+
+// @ts-check
+/**
+ *
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+function sumJSDOC(a, b) {
+  return a + b;
+}
+
+var resultJSDOC = sumJSDOC(10, 20);
