c6d435a3ded6438cb7af98bc4c92b2605f581d8f
kkuyu <devils67@naver.com>
2022-03-05 18:19:40 +0900

    note: 기본 타입 - 튜플, 객체, 진위값

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -4,3 +4,11 @@ let num: number = 10;
 let arr: Array<number> = [1, 2, 3];
 let heros: Array<string> = ["A", "B", "C"];
 let items: number[] = [1, 2, 3];
+
+let obj: object = {};
+let person: { name: string; age: number } = {
+  name: "thor",
+  age: 1000,
+};
+let address: [string, string, number] = ["gangnam", "pangyo", 100];
+let show: boolean = true;
