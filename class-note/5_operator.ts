795d93eedaee5fcb64df122ce9a129a9154d8f5c
kkuyu <devils67@naver.com>
2022-03-08 19:25:10 +0900

    note: 유니온 타입과 인터섹션 타입의 차이점

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -27,3 +27,14 @@ function askSomeone(someone: Developer | Person) {
   // console.log(someone.skill);
   // console.log(someone.age);
 }
+
+askSomeone({ name: "foo", skill: "js" });
+askSomeone({ name: "bar", age: 10 });
+
+function askSomebody(someone: Developer & Person) {
+  console.log(someone.name);
+  // console.log(someone.skill);
+  // console.log(someone.age);
+}
+
+askSomebody({ name: "baz", skill: "js", age: 10 });
