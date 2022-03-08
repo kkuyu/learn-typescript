46286cf0d88274d760c51e2a8966412b5296140c
kkuyu <devils67@naver.com>
2022-03-08 19:17:37 +0900

    note: 유니온 타입의 특징

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -11,3 +11,19 @@ function logMessage(value: string | number) {
 
 logMessage("hello");
 logMessage(100);
+
+interface Developer {
+  name: string;
+  skill: string;
+}
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+function askSomeone(someone: Developer | Person) {
+  console.log(someone.name);
+  // console.log(someone.skill);
+  // console.log(someone.age);
+}
