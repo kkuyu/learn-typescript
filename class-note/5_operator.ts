ab1d1eba310486a2503d25e82a4e25ff923a0d9d
kkuyu <devils67@naver.com>
2022-03-08 19:06:42 +0900

    note: 유니온 타입의 장점, 타입 가드

--- class-note/5_operator.ts
+++ class-note/5_operator.ts
@@ -1,5 +1,12 @@
 function logMessage(value: string | number) {
   console.log(value);
+  if (typeof value === "number") {
+    value.toLocaleString();
+  }
+  if (typeof value === "string") {
+    value.toString();
+  }
+  throw new TypeError("value must be string or number");
 }
 
 logMessage("hello");
