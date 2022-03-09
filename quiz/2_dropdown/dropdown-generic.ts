92a77c25a39aee36da60cf118c9333b20a987e7a
kkuyu <devils67@naver.com>
2022-03-09 19:22:17 +0900

    dropdown-generic: 제네릭 타입 정의하기

--- quiz/2_dropdown/dropdown-generic.ts
+++ quiz/2_dropdown/dropdown-generic.ts
@@ -1,26 +1,21 @@
-interface Email {
-  value: string;
+interface DropdownItem<T> {
+  value: T;
   selected: boolean;
 }
 
-interface ProductNumber {
-  value: number;
-  selected: boolean;
-}
-
-const emails: Email[] = [
+const emails: DropdownItem<string>[] = [
   { value: "naver.com", selected: true },
   { value: "gmail.com", selected: false },
   { value: "hanmail.net", selected: false },
 ];
 
-const numberOfProducts: ProductNumber[] = [
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item: Email | ProductNumber) {
+function createDropdownItem<T>(item: DropdownItem<T>) {
   const option = document.createElement("option");
   option.value = item.value.toString();
   option.innerText = item.value.toString();
@@ -30,7 +25,7 @@ function createDropdownItem(item: Email | ProductNumber) {
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
+  const item = createDropdownItem<string>(email);
   const selectTag = document.querySelector("#email-dropdown");
   selectTag.appendChild(item);
 });
