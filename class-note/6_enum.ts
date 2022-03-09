1456d003bc4935943900018d5ccbdd95bf7452b5
kkuyu <devils67@naver.com>
2022-03-09 18:11:31 +0900

    note: 문자형 이넘

--- class-note/6_enum.ts
+++ class-note/6_enum.ts
@@ -5,3 +5,11 @@ enum Shoes {
 
 let myShoes = Shoes.Nike;
 console.log(myShoes); // 0
+
+enum Brand {
+  Nike = "나이키",
+  Adidas = "아디다스",
+}
+
+var myBrand = Brand.Nike;
+console.log(myBrand); // '나이키'
