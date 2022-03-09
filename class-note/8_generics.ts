0d9b76b2fb64130451fca6b59420c81e57cf1491
kkuyu <devils67@naver.com>
2022-03-09 19:06:26 +0900

    note: 인터페이스에 제네릭을 선언하는 방법

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -4,3 +4,18 @@ function logText<T>(text: T): T {
 
 logText(10);
 logText<string>("hello");
+
+interface Dropdown<T> {
+  value: T;
+  selected: boolean;
+}
+
+const stringObj: Dropdown<string> = {
+  value: "abc",
+  selected: false,
+};
+
+const numberObj: Dropdown<number> = {
+  value: 10,
+  selected: false,
+};
