82e9868edb8e1048b331ae09ba354d64e67fa2f5
kkuyu <devils67@naver.com>
2022-03-12 18:25:21 +0900

    note: 인터페이스와 제네릭을 이용한 타입 추론 방식

--- class-note/9_type-inference.ts
+++ class-note/9_type-inference.ts
@@ -4,3 +4,13 @@ function getB(b: number = 20): string {
   var c: string = "hi";
   return b + c;
 }
+
+interface Dropdown<T> {
+  value: T;
+  title: string;
+}
+
+var shoppingItem: Dropdown<string> = {
+  value: "abc",
+  title: "abc",
+};
