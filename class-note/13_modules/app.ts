15f69f6216d1b7db42ba4b2efc4680355b017adb
kkuyu <devils67@naver.com>
2022-03-12 19:20:16 +0900

    note: module

---
+++ class-note/13_modules/app.ts
@@ -0,0 +1,10 @@
+import Test, { Todo } from "./types";
+
+var test: Test = {
+  foo: 123,
+};
+
+var item: Todo = {
+  title: "할일1",
+  checked: false,
+};
---
+++ class-note/13_modules/types.ts
@@ -0,0 +1,8 @@
+export default interface Test {
+  foo: number;
+}
+
+export interface Todo {
+  title: string;
+  checked: boolean;
+}
