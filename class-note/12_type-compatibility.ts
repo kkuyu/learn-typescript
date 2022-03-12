c9dc1e3d7ed05d52cfc5dd0ea79e598de8fab59d
kkuyu <devils67@naver.com>
2022-03-12 19:15:08 +0900

    note: 타입 호환 - 함수, 제네릭

--- class-note/12_type-compatibility.ts
+++ class-note/12_type-compatibility.ts
@@ -8,3 +8,26 @@ class Avengers {
 
 let i: Ironman;
 i = new Avengers();
+
+var add = function (a: number) {};
+var sum = function (a: number, b: number) {};
+
+// sum = add;
+
+interface Empty<T> {}
+
+var empty1: Empty<string>;
+var empty2: Empty<number>;
+
+// empty1 = empty2;
+// empty2 = empty1;
+
+interface NotEmpty<T> {
+  data: T;
+}
+
+var notEmpty1: NotEmpty<string>;
+var notEmpty2: NotEmpty<number>;
+
+// notEmpty1 = notEmpty2
+// notEmpty2 = notEmpty1
