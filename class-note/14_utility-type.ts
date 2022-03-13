f71fb033bfbe4c14a93a7f5c788c7270f9f22c7a
kkuyu <devils67@naver.com>
2022-03-13 18:50:52 +0900

    note: 유틸리티 타입 구현

--- class-note/14_utility-type.ts
+++ class-note/14_utility-type.ts
@@ -39,3 +39,33 @@ type MayHaveEmail = Partial<Address>;
 const me: MayHaveEmail = {};
 const you: MayHaveEmail = { email: "test@abc.com" };
 const all: MayHaveEmail = { email: "capt@hero.com", address: "Pangyo" };
+
+interface UserProfile {
+  username: string;
+  email: string;
+  profilePhotoUrl: string;
+}
+
+interface MoreUserProfileA {
+  username?: string;
+  email?: string;
+  profilePhotoUrl?: string;
+}
+
+type MoreUserProfileB = {
+  username?: UserProfile["username"];
+  email?: UserProfile["email"];
+  profilePhotoUrl?: UserProfile["profilePhotoUrl"];
+};
+
+type MoreUserProfileC = {
+  [p in "username" | "email" | "profilePhotoUrl"]?: UserProfile[p];
+};
+
+type MoreUserProfileD = {
+  [p in keyof UserProfile]?: UserProfile[p];
+};
+
+type MoreUserProfileE<T> = {
+  [p in keyof T]?: T[p]; // same as Partial
+};
