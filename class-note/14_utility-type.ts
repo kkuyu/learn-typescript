641dddcfbf23b94eff74d8fdb3143d06fa5e8cb7
kkuyu <devils67@naver.com>
2022-03-13 18:37:15 +0900

    note: 유틸리티 타입 사례 - Omit

--- class-note/14_utility-type.ts
+++ class-note/14_utility-type.ts
@@ -11,3 +11,21 @@ function fetchProducts(): Promise<Product[]> {}
 
 type ShoppingItem = Pick<Product, "id" | "name" | "price">;
 function displayProductDetail(shoppingItem: ShoppingItem) {}
+
+interface AddressBook {
+  name: string;
+  phone: number;
+  address: string;
+  company: string;
+}
+
+const phoneBook: Omit<AddressBook, "address"> = {
+  name: "재택근무",
+  phone: 12342223333,
+  company: "내 방",
+};
+
+const chingtao: Omit<AddressBook, "address" | "company"> = {
+  name: "중국집",
+  phone: 44455557777,
+};
