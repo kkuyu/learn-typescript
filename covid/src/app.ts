203876cb8924208b01a647f5f652047a84874715
kkuyu <devils67@naver.com>
2022-03-15 18:46:17 +0900

    covid: API 함수 타입 구체화

--- covid/src/app.ts
+++ covid/src/app.ts
@@ -38,7 +38,13 @@ function fetchCovidSummary() {
   return axios.get(url);
 }
 
-function fetchCountryInfo(countryCode: any, status: any) {
+enum CovidStatus {
+  Confirmed = "confirmed",
+  Recovered = "recovered",
+  Deaths = "deaths",
+}
+
+function fetchCountryInfo(countryCode: string, status: CovidStatus) {
   // params: confirmed, recovered, deaths
   const url = `https://api.covid19api.com/country/${countryCode}/status/${status}`;
   return axios.get(url);
