4b995754c55e264f31400ab14a53d723b883cf41
kkuyu <devils67@naver.com>
2022-03-16 18:28:17 +0900

    covid: ESLint 설정

---
+++ covid/.eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/eslint-recommended", "plugin:@typescript-eslint/recommended"],
+  plugins: ["prettier", "@typescript-eslint"],
+  rules: {},
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+};
